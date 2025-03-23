b4a68e227291ef4e13c818abebf24f1b443727ce
lexiscode <nwokoriealex20@gmail.com>
2025-03-23 11:42:44 +0100

    added the actual project file

--- index.php
+++ index.php
@@ -1,171 +1,204 @@
 <?php
 
-// Define the file path
-$filePath = 'todo.txt';
+// Function to load the todo list from JSON file
+function loadRecordList($filename) {
+    if (file_exists($filename)) {
+        $json = file_get_contents($filename);
+        $recordList = json_decode($json, true);
 
-// Check if the file exists, and create it if it doesn't
-if (!file_exists($filePath)) {
-    $file = fopen($filePath, 'w');
-    fclose($file);
-}
-
-// Function to read the todo list from the file to an array
-function readTodoList()
-{
-    global $filePath;
-    $todoList = []; // initializes the array as being empty
-
-    $file = fopen($filePath, 'r'); // opens the file first and granting read permission
-    while (($line = fgets($file)) !== false) { 
-        $todoList[] = trim($line); // gets the data values for each lines in the file and then add it into the array
+    } else {
+        $recordList = [];
     }
-    fclose($file);
 
-    return $todoList;
+    return $recordList;
 }
 
-// Function to write the todo list from an array to the file
-function writeTodoList($todoList)
-{
-    global $filePath;
-
-    $file = fopen($filePath, 'w'); // opens the file first and granting write permission
-    foreach ($todoList as $todo) {
-        fwrite($file, $todo . PHP_EOL);  // writes data values loop by loop into the file
-    }
-    fclose($file);
+// Function to save the todo list to JSON file
+function saveRecordList($filename, $recordList) {
+    $json = json_encode($recordList); 
+    file_put_contents($filename, $json);
 }
 
-// Add a task to the todo list
-if (isset($_POST['addTask'])) {  // checks if the button is pushed/clicked, which will turn to "True"
-    $task = trim($_POST['task']); // the value of the input is received due to the pushed button
+// Set the filename for the todo list JSON file
+$filename = 'record_list.json';
 
-    if (!empty($task)) {
-        $todoList = readTodoList(); //bringing out the list from the .txt file to array
-        $todoList[] = $task; // adding to the array list
-        writeTodoList($todoList); // taking it back (overwriting/updating) into the .txt file
+// Load th from JSON file
+$recordList = loadRecordList($filename);
+
+// Check if a new task is submitted and its not empty, then add it to the 
+if (isset($_POST['save'])){
+    if (!empty($_POST['level']) && !empty($_POST['course'])){
+        $level = $_POST['level']; 
+        $course = $_POST['course']; 
+
+        // Create a new task array 
+        $newRecord = ['level' => $level, 'course' => $course];
+
+        // Add the new task to the 
+        $recordList[] = $newRecord;
+
+        // Save the updated
+        saveRecordList($filename, $recordList);
     }
 }
 
-// Deletes a task from the todo list
-if (isset($_POST['removeTask'])) {  // checks if the button is pushed/clicked, which will turn to "True"
-    $index = $_POST['index']; // the value of the input is received due to the pushed button
+// Check if an updated task is submitted
+if (isset($_POST['edit_index']) && isset($_POST['updateLevel']) && isset($_POST['updateCourse'])) {
+    $editIndex = $_POST['edit_index']; // Get the index
+    $updateLevel = $_POST['updateLevel']; 
+    $updateCourse = $_POST['updateCourse']; 
 
-    $todoList = readTodoList(); //bringing out the list from the .txt file to an array
-    if (isset($todoList[$index])) { // if the key/index exists in this array
-        unset($todoList[$index]); // remove the specified data (in that index) from the array list
-        writeTodoList($todoList); // taking the latest/updated array list back into the .txt file
+    // Update the task and due date
+    if (isset($recordList[$editIndex])) { // checks/ensures if there is a value where u wish to update
+        $recordList[$editIndex]['level'] = $updateLevel; 
+        $recordList[$editIndex]['course'] = $updateCourse; 
+
+        // Save the updated record list
+        saveRecordList($filename, $recordList);
     }
 }
 
-// Update the todo list
-if (isset($_POST['index']) && isset($_POST['updated_task'])) {
-    $editIndex = $_POST['index'];
-    $updatedTask = $_POST['updated_task'];
-
-    $todoList = readTodoList(); //bringing out the list from the .txt file to array
-
-    if (isset($todoList[$editIndex])) { 
-        $todoList[$editIndex] = $updatedTask; // Update or overwrite the specific task
-        writeTodoList($todoList); // Save the updated todo list back to the txt file
-    }
-}
-
-// Clear the todo list and delete file content
-if (isset($_POST['clearLists'])) {
-    $todoList = [];
-    writeTodoList($todoList);
-}
 
 // Numbering the ToDo list
-function taskNumber(){
+function recordNumber(){
     static $a = 1;
     echo $a;
     $a++;
 }
 
-// Retrieve the current todo list from the file to an array
-$todoList = readTodoList(); // its this current list we are viewing in our html page
-
 ?>
 
-<!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TODO-LIST APP</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+<body>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Add Student Record
+    </button>
 
-    <div class="container">
-        <!--Introduction header-->
-        <h1 class="text-center my-4 py-4" style="font-family: Tahoma, Verdana, Segoe, sans-serif">Welcome To My ToDo List</h1>
+    
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <form action="" method="POST">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Input Your Data</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
 
-        <!--First Top Form-->
-        <div class="w-50 m-auto">
-            <form action="" method="POST" autocomplete="off">
-                <label for="title">Task:</label>
-                <input class="form-control" type="text" name="task" id="task" placeholder="Enter Task To Add in Todo" required>
-                <br>
-                <button type="submit" class="btn btn-success" name="addTask">Add To ToDo</button>
-                <button type="submit" class="btn btn-secondary" name="clearLists">Clear Lists</button>
-            </form>
+                            <label for="">Your School Level:</label>
+                            <select class="form-select" aria-label="Default select example" name="level">
+                                <option value="100L">100L</option>
+                                <option value="200L" selected>200L</option>
+                            </select>
+                            <br>
+
+                            <label for="">Course of Study:</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="Computer Science" name="course"  id="flexRadioDefault1" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Computer Science
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="Computer Engineering" name="course" id="flexRadioDefault2">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Computer Engineering
+                                </label>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name="save">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-        <br>
+        <hr>
+        <br> <br>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Student Level</th>
+                <th scope="col">Course of Study</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <?php if (!empty($recordList)) : ?>
+            <tbody>
+                <?php foreach ($recordList as $index => $record) : ?>
+                <tr>
+                <th scope="row"><?php recordNumber()?></th>
+                <td><?php echo $record['level']; ?></td>
+                <td><?php echo $record['course']; ?></td>
+                <!-- Update section -->
+                <form action="" method="POST">
+                    <input type="hidden" name="edit_index" value="<?php echo $index; ?>">
+                    <td>
+                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?php echo $index; ?>">
+                        Update
+                        </button>
+                    </td>
 
-        <!--Horizontal line demacation-->
-        <hr class="bg-dark w-50 m-auto">
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop<?php echo $index; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel<?php echo $index; ?>" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            
+                            <label for="">Your School Level:</label>
+                            <select class="form-select" aria-label="Default select example" name="updateLevel">
+                                <option value="100L" <?php echo $record['level'] == '100L' ? 'selected' : '';?>>100L</option>
+                                <option value="200L" <?php echo $record['level'] == '200L' ? 'selected' : '';?>>200L</option>
+                            </select>
+                            <br>
 
-        <!-- Table -->
-        <div class="w-50 m-auto">
-            <h1>Your Lists</h1>
+                            <label for="">Course of Study:</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="Computer Science" name="updateCourse"  id="flexRadioDefault1" <?php echo $record['course'] == 'Computer Science' ? 'checked' : '';?>>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Computer Science
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="Computer Engineering" name="updateCourse" id="flexRadioDefault2" <?php echo $record['course'] == 'Computer Engineering' ? 'checked' : '';?>>
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Computer Engineering
+                                </label>
+                            </div>
 
-            <table class="table table-dark table-hover">
-                <thead style="text-align: center;">
-                    <tr>
-                    <th scope="col">S/N</th>
-                    <th scope="col">Tasks</th>
-                    <th scope="col">Action</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Action</th>
-                    </tr>
-                </thead>
-
-                <?php if (!empty($todoList)) : ?>
-
-                <tbody style="text-align: center;">
-                    <?php foreach ($todoList as $index => $task) : ?>
-                        <tr>
-                            <td><?php taskNumber()?></td>
-                            <td id="toggle"><?php echo $task; ?></td>
-                            <td>
-                                <form method="POST" action="">
-                                    <input type="hidden" name="index" value="<?php echo $index; ?>">
-                                    <button type="submit" name="removeTask" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
-                            </td>
-                            <!-- Update section -->
-                            <form action="" method="POST">
-                                <input type="hidden" name="index" value="<?php echo $index; ?>">
-                                <td><input class="form-control" type="text" name="updated_task" id="updated_task" size="5" placeholder="Update Task" required></td>
-                                <td><button type="submit" class="btn btn-primary btn-sm">Update</button></td>
-                            </form>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-
-                <?php else : ?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Update Record</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </form>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+            <?php else : ?>
                     <p>No lists found.</p>
                 <?php endif; ?>
-
             </table>
-        </div>
-        
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-  </body>
+</body>
 </html>
